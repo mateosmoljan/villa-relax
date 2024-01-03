fe8f79d8305b37aed18ca9a935e8fb2c36ac63e4
Sudo <93739034+SudodoSu@users.noreply.github.com>
2024-01-03 19:27:26 +0100

    style: remove unnecessary spacing in code

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import { images } from "@/lib/images";
 import { EffectFade } from "swiper/modules";
