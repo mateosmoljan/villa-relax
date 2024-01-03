9a08a4333a0ea47576b52b9bbca33a7920aab395
Sudo <93739034+SudodoSu@users.noreply.github.com>
2024-01-03 20:05:33 +0100

    fix: useEffect

--- components/FullscreenButton.tsx
+++ components/FullscreenButton.tsx
@@ -1,25 +1,37 @@
+"use client";
+import { useEffect } from "react";
+
 const FullscreenButton = () => {
   const handleFullscreen = () => {
-    if (typeof document !== "undefined") {
-      const element = document.documentElement;
-
-      if (document.fullscreenElement) {
-        document.exitFullscreen();
-      } else {
-        element.requestFullscreen().catch((err) => {
-          console.error(
-            `Error attempting to enable fullscreen: ${err.message}`
-          );
-        });
-      }
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      const fullscreenElement = document.documentElement;
+      fullscreenElement.requestFullscreen().catch((err) => {
+        console.error(`Error attempting to enable fullscreen: ${err.message}`);
+      });
     }
   };
 
+  const handleClick = () => {
+    handleFullscreen();
+  };
+
+  useEffect(() => {
+    document.addEventListener("fullscreenchange", () => {
+      // Handle changes to fullscreen state if needed
+    });
+
+    return () => {
+      document.removeEventListener("fullscreenchange", () => {
+        // Clean up event listener if needed
+      });
+    };
+  }, []); // Empty dependency array ensures that this effect runs only once
+
   return (
-    <button onClick={handleFullscreen}>
-      {typeof document !== "undefined" && document.fullscreenElement
-        ? "Exit Fullscreen"
-        : "Go Fullscreen"}
+    <button onClick={handleClick}>
+      {document.fullscreenElement ? "Exit Fullscreen" : "Go Fullscreen"}
     </button>
   );
 };
