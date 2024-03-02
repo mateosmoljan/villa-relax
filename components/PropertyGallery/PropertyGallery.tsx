0f2ed5ceb8348fca98add21290ab563eb5d1051a
SudoDosu <sudodosu99@gmail.com>
2024-03-02 01:44:03 +0100

    fix

--- components/PropertyGallery/PropertyGallery.tsx
+++ components/PropertyGallery/PropertyGallery.tsx
@@ -17,7 +17,7 @@ import { SwiperNavButtons } from "./SwiperNavButton";
 import Loading from "../Loading/Loading";
 
 export default function PropertyGallery() {
-  const [thumbsSwiper, setThumbsSwiper] = useState<typeof Swiper | null>(null);
+  const [thumbsSwiper, setThumbsSwiper] = React.useState();
   const [activeIndex, setActiveIndex] = useState<number>(0);
   const [isLoaded, setIsLoaded] = useState(false);
 
@@ -33,7 +33,10 @@ export default function PropertyGallery() {
             effect={"fade"}
             spaceBetween={10}
             navigation={true}
-            thumbs={{ swiper: thumbsSwiper }}
+            thumbs={{
+              swiper:
+                thumbsSwiper && !thumbsSwiper?.destroyed ? thumbsSwiper : null,
+            }}
             modules={[FreeMode, Navigation, Thumbs, EffectFade]}
             onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
             className="PropertySwiper !overflow-visible"
