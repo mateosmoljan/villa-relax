dbddc58cdc6ab4fb74433dab96a16792ac0a78b9
SudoDosu <93739034+SudodoSu@users.noreply.github.com>
2024-05-17 17:22:48 +0200

    chore

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "www.villarelaxpula.com",
+        destination: "/",
         permanent: true,
         basePath: false,
       },
